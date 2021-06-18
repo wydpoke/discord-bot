4f69011abe7c0265edf22d9b5c86e0d0c618c92c
Josh <35877820+wydpoke@users.noreply.github.com>
2021-06-18 16:26:27 +0100

    Added remove role command

--- commands/removerole.js
+++ commands/removerole.js
@@ -1,14 +1,13 @@
 module.exports.run = async (bot, msg, args) => {
-    let target = msg.mentions.members.first() ||  args[0]
+    let target = msg.mentions.members.first() || args[0]
+    let arg = msg.content.split(" ")
 
     try {
-        let role = msg.guild.roles.cache.find(r => r.name === args.join(' '))
+        let role = msg.guild.roles.cache.find(r => r.name.toLowerCase() === arg[2])
         if(!role) return msg.channel.send('Role doesn\'t exist')
-
-        console.log(args.join(' '))
         
-        target.roles.remove(role)
-        await msg.channel.send(`Removed ${role} from ${target}`)
+        await target.roles.remove(role)
+        msg.channel.send(`Removed ${role} from ${target}`)
     } catch(e) {
         msg.channel.send(`\`Error:\` \`\`\`js\n${e}\n\`\`\``)
     }
